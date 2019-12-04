849e815d318d8efcacf2526eecd5858b73b0a76c
Leonardo Cserny <leonardo.cserny@endava.com>
2019-12-04 16:54:27 +0200

    Add project files.

---
+++ Data/SeedData.cs
@@ -0,0 +1,32 @@
+﻿using ContosoPets.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoPets.Api.Data
+{
+    public class SeedData
+    {
+        public static void Initialize(ContosoPetsContext context)
+        {
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Squeaky Bone",
+                        Price = 20.99m
+                    },
+                    new Product
+                    {
+                        Name = "Knotted Rope",
+                        Price = 12.99m
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
